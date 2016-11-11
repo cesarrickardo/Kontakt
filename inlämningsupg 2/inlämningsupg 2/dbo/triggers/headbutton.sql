70c5eebfcb9a1467b6f20bd0f6c580f408a78cdf
Ricky Rock <ricky_rickardo87@hotmail.com>
2016-11-11 15:51:51 +0100

    trigger

---
+++ inlämningsupg 2/inlämningsupg 2/dbo/triggers/headbutton.sql
@@ -0,0 +1,23 @@
+﻿
+CREATE TRIGGER [dbo].[TestNewTrigger] 
+   ON  [dbo].[Customers]
+   AFTER UPDATE
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from 
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	INSERT INTO tblCustomerChanges
+        ([CustomerID], [ContactNameOLD],[ContactNameNEW])
+ select i.CustomerID, d.ContactName, i.ContactName
+ from inserted i
+ inner join deleted d on i.CustomerID= d.CustomerID
+ where d.ContactName <> i.ContactName
+
+    -- Insert statements for trigger here
+END
+GO
+
+ALTER TABLE [dbo].[Customers] ENABLE TRIGGER [TestNewTrigger]
+GO
+
